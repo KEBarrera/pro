23b1d0a1eaed1472995fc0d6c1b11e283bf81789
KEBarrera <karlabarrera992@gmail.com>
2023-05-02 11:31:57 -0500

    Version 1.0

---
+++ OneDrive/Documentos/NetBeansProjects/PROYECTO_KBARRERA/src/ec/edu/intsuperior/vista/JavaApplication219.java
@@ -0,0 +1,47 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package ec.edu.intsuperior.vista;
+
+/**
+ *
+ * @author Lenovo
+ */
+        
+        import java.util.Scanner;
+ 
+import java.util.Scanner;
+ 
+public class JavaApplication219 {
+ 
+     
+    public static void main(String[] args) {
+         
+       Scanner sn = new Scanner(System.in);
+       boolean salir = false;
+       int opcion; //Guardaremos la opcion del usuario
+        
+       while(!salir){
+            
+           System.out.println("1. Opcion 1");
+           System.out.println("2. Opcion 2");
+           System.out.println("3. Opcion 3");
+           System.out.println("4. Salir");
+            
+           System.out.println("Escribe una de las opciones");
+           opcion = sn.nextInt();
+            
+       }
+        
+    }
+     
+}
+   
+     
+
+  
+
+
+    
+
