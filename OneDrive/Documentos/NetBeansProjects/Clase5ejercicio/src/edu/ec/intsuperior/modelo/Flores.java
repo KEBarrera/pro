f687392c75fc0fe732de1ffd95b0d79c93ce274a
KEBarrera <karlabarrera992@gmail.com>
2023-06-15 18:03:17 -0500

    EJERCICIOS 5 CLASES

--- OneDrive/Documentos/NetBeansProjects/Clase5ejercicio/src/edu/ec/intsuperior/modelo/Flores.java
+++ OneDrive/Documentos/NetBeansProjects/Clase5ejercicio/src/edu/ec/intsuperior/modelo/Flores.java
@@ -1,3 +1,5 @@
+package edu.ec.intsuperior.modelo;
+
 /*
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
